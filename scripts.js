6a79116c872767def88ecdabce375598f4fb5d4c
Your Name <cosmin.ciurevici2017@gmail.com>
2026-06-11 22:05:00 +0300

    added basic movable window

--- scripts.js
+++ scripts.js
@@ -10,3 +10,69 @@ document.getElementById("clock").innerHTML = zeclock;
 timeUpdate();
 setInterval(timeUpdate, 1000);
 
+
+// Make the DIV element draggable:
+dragElement(document.getElementById("welcome"));
+
+function dragElement(elmnt) {
+  var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
+  if (document.getElementById(elmnt.id + "header")) {
+
+    document.getElementById(elmnt.id + "header").onmousedown = dragMouseDown;
+    } else {
+    elmnt.onmousedown = dragMouseDown;
+  }
+
+function dragMouseDown(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // get the mouse cursor position at startup:
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    document.onmouseup = closeDragElement;
+    // call a function whenever the cursor moves:
+    document.onmousemove = elementDrag;
+  }
+
+  function elementDrag(e) {
+    e = e || window.event;
+    e.preventDefault();
+    // calculate the new cursor position:
+    pos1 = pos3 - e.clientX;
+    pos2 = pos4 - e.clientY;
+    pos3 = e.clientX;
+    pos4 = e.clientY;
+    // set the element's new position:
+    elmnt.style.top = (elmnt.offsetTop - pos2) + "px";
+    elmnt.style.left = (elmnt.offsetLeft - pos1) + "px";
+  }
+
+  function closeDragElement() {
+    // stop moving when mouse button is released:
+    document.onmouseup = null;
+    document.onmousemove = null;
+  }
+}
+
+
+
+var welcomeScreen = document.getElementById("welcome");
+
+function closeWindow(element) {
+    element.style.display = "none";
+}
+
+function openWindow(element) {
+  element.style.display = "flex"
+}
+
+var welcomeScreenClose = document.querySelector("#welcomeclose")
+var welcomeScreenOpen = document.querySelector("#welcomeopen")
+
+welcomeScreenClose.addEventListener("click", function() {
+  closeWindow(welcomeScreen);
+});
+
+welcomeScreenOpen.addEventListener("click", function() {
+  openWindow(welcomeScreen);
+});
